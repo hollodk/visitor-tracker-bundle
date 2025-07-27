e12c74e4efb24b2fc67278a9c4414d48f411e01c
Michael Holm <hollo@hollo.dk>
2025-07-27 19:06:21 +0200

    Added better monitoring

--- BeastVisitorTrackerBundle.php
+++ BeastVisitorTrackerBundle.php
@@ -5,7 +5,7 @@ namespace Beast\VisitorTrackerBundle;
 use Beast\VisitorTrackerBundle\DependencyInjection\BeastVisitorTrackerExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class VisitorTrackerBundle extends Bundle
+class BeastVisitorTrackerBundle extends Bundle
 {
     public function getContainerExtension(): ?\Symfony\Component\DependencyInjection\Extension\ExtensionInterface
     {
